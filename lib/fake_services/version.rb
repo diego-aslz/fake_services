8d04e44a970c5fc3fa2b642629be0af0d9b49a97
Diego Selzlein <diegoselzlein@gmail.com>
2022-03-03 23:10:06 -0300

    Initial setup.

---
+++ lib/fake_services/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FakeServices
+  VERSION = "0.1.0"
+end
---
+++ spec/fake_services_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe FakeServices do
+  it "has a version number" do
+    expect(FakeServices::VERSION).not_to be nil
+  end
+end
